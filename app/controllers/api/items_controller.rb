61e3cf3a393f7822638696c20bec1b390a075dd6
Ian Harwell <sirharwell@gmail.com>
2018-03-08 10:13:05 -0700

    react

--- app/controllers/api/items_controller.rb
+++ app/controllers/api/items_controller.rb
@@ -1,4 +1,5 @@
 class Api::ItemsController < ApplicationController
+  before_action :set_item, only: [:update, :destroy]
   def index
     render json: Item.all
   end
@@ -10,5 +11,23 @@ class Api::ItemsController < ApplicationController
       render json: item
     else
       render json: { errors: item.errors }, status: 422
+    end
+
+    def update
+      @item.update(complete: !@item.complete)
+      render json: @item
+    end
+
+    def destroy
+      @item.destroy
+    end
+
+    private
+    def item_params
+      params.require(:item).permit(:name, :complete)
+    end
+
+    def set_item
+      @item = Item.find(params[:id])
     end
 end
---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import TodoForm from './components/TodoForm';
+import TodoList from './components/TodoList';
+
+
+
+class App extends Component {
+  render() {
+    return (
+    );
+  }
+}
+
+export default App;
